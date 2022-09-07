57a9cca8fa3bb5601229a4bc849877243cb73c37
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-09-07 10:35:39 -0500

    mvp

--- bd/conn.js
+++ bd/conn.js
@@ -1,9 +1,18 @@
-const {Pool} = require("pg")
+// const {Pool} = require("pg")
 
-const pool = new Pool({
-    user: "justi",
-    password: "Justin#127166",
-    host: "localhost",
-    port: 5432,
-    database: "list"
-});
+// const pool = new Pool({
+//     user: "justi",
+//     password: "Justin#127166",
+//     host: "localhost",
+//     port: 5432,
+//     database: "list"
+// });
+
+
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
--- bd/goals.sql
+++ bd/goals.sql
@@ -2,7 +2,5 @@ DROP TABLE IF EXISTS goals;
 
 CREATE TABLE goals(
 id serial,
-user_name text,
-goal text,
-time_frame text
+goal text
 );
